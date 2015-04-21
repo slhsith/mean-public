cfe17ddbe2ab8457110120f78ff97c38978c2902
Leo Schultz <schultz.leo@gmail.com>
2015-04-20 17:15:40 -0700

    lots of structure changes

---
+++ client/admin/js/models.js
@@ -0,0 +1,86 @@
+app.factory('posts', ['$http', 'auth', function($http, auth){
+  var o = {
+    posts: []
+  };
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+  o.create = function(post) {
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      post.upvotes += 1;
+    });
+  };
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    });
+  };
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      comment.upvotes += 1;
+    });
+  };
+  return o;
+}]);
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+   var auth = {};
+   auth.saveToken = function (token){
+      $window.localStorage['flapper-news-token'] = token;
+    };
+
+    auth.getToken = function (){
+      return $window.localStorage['flapper-news-token'];
+    };
+    auth.isLoggedIn = function(){
+      var token = auth.getToken();
+
+      if(token){
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+    auth.currentUser = function(){
+      if(auth.isLoggedIn()){
+        var token = auth.getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.username;
+      }
+    };
+    auth.register = function(user){
+      return $http.post('/register', user).success(function(data){
+        auth.saveToken(data.token);
+      });
+    };
+    auth.logIn = function(user){
+      return $http.post('/login', user).success(function(data){
+        auth.saveToken(data.token);
+      });
+    };
+    auth.logOut = function(){
+      $window.localStorage.removeItem('flapper-news-token');
+    };
+  return auth;
+}]);
---
+++ client/user/js/controllers.js
@@ -0,0 +1,77 @@
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+'auth',
+function($scope, posts, auth){
+  $scope.posts = posts.posts;
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link,
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+  $scope.incrementUpvotes = function(post) {
+  posts.upvote(post);
+  };
+  $scope.isLoggedIn = auth.isLoggedIn;
+}]);
+app.controller('PostsCtrl', [
+'$scope',
+'posts',
+'post',
+'auth',
+function($scope, posts, post, auth){
+  $scope.posts = posts.posts;
+  $scope.post = post;
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    posts.addComment(post._id, {
+      body: $scope.body,
+      author: 'user',
+    }).success(function(comment) {
+      $scope.post.comments.push(comment);
+    });
+    $scope.body = '';
+
+  };
+  $scope.incrementUpvotes = function(comment){
+    posts.upvoteComment(post, comment);
+  };
+}]);
+
+
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}]);
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
