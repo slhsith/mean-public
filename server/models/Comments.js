2bbbe997d687ada1b71ae26b1e5917a9e7b029f7
leorue <schultz.leo@gmail.com>
2015-04-26 23:31:01 -0700

    Getting on the right track

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -6,6 +6,7 @@ var CommentSchema = new mongoose.Schema({
   upvotes: {type: Number, default: 0},
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
+
 CommentSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
   this.save(cb);
