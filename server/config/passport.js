77a497bcb19ec7d951c3dbfe0598d72b5de17ade
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-04-28 12:29:00 -0400

    Stupid register

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,13 +5,13 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
+  function (username, password, done) {
     User.findOne({ username: username }, function (err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect email.' });
       }
-      if(!user.validPassword(password)) {
+      if (!user.validPassword(password)) {
         return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
@@ -25,7 +25,7 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:3000/auth/facebook/callback",
     enableProof: false
   },
-  function(accessToken, refreshToken, profile, done) {
+  function (accessToken, refreshToken, profile, done) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
       return done(err, user);
     });
