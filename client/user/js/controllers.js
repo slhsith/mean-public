2eda983aec5b68689ff62b38e301afdda50123ad
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-04-29 15:42:41 -0400

    added image crop

--- client/user/js/controllers.js
+++ client/user/js/controllers.js
@@ -82,4 +82,18 @@ function($scope, auth){
 app.controller('SettingsCtrl', [
 '$scope',
 function($scope){
+  $scope.myImage='';
+  $scope.myCroppedImage='';
+
+  var handleFileSelect=function(evt) {
+    var file=evt.currentTarget.files[0];
+    var reader = new FileReader();
+    reader.onload = function (evt) {
+      $scope.$apply(function($scope){
+        $scope.myImage=evt.target.result;
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+  angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
 }]);
