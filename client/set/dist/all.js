13b13c725592d8806f22b51d4fae514d2ccc23c3
Leo Schultz <schultz.leo@gmail.com>
2015-04-30 17:35:19 -0700

    working??

--- client/set/dist/all.js
+++ client/set/dist/all.js
@@ -28,44 +28,36 @@ app.controller('MainCtrl', ['$scope', '$location', function ($scope) {
   // };
 
 }]);
-app.controller('ResetCtrl', ['$scope', '$location','$state', '$stateParams', 'resetPassword', function ($scope, $state, $stateParams, reset) {
+app.controller('ResetCtrl', function ($scope, $state, $stateParams, verification) {
   $scope.submitPassword = function() {
     console.log($stateParams.params.username);
     console.log($stateParams.params.token);
     console.log($scope.user.password);
-    // console.log($stateParams.username);
-    // console.log($stateParams.token);
-    reset.resetPassword.updatePassword(user, $stateParams.params.username, $stateParams.params.token)
-      .success(function () {
-      // output message
-      // redirect them home
-      });
-    // .error(function (error) {
-    //   $scope.error = error;
-    //   $scope.showSuccessAlert = true;
-    // });
+    verification.updatePassword(user, $stateParams.params.username, $stateParams.params.token).success(function () {
+      // redirect home
+      //
+    }).error(function (error) {
+      $scope.error = error;
+      $scope.showSuccessAlert = true;
+    });
   }; 
-}]);
-// app.factory('confirmEmail',['$http', '$window', function ($http, $window) {
-//   var confirmEmail = {};
-//   confirmEmail.confirm = function (user) {
-//     return $http.put('/api/emailverify/:username/:token').success(function (data) {
-//     });
-//   };
-//   return confirmEmail;
-// }]);
+});
+app.factory('verification', function ($http, $window) {
+  return {
+      getUser: function getUserMethod(user, name, token) {
+          return $http.get('/resetPassword/'+ name + '/' + token).success(function (data) {
+            return data;
+          });
+      },
+      emailVerify: function emailVerifyMethod(user, name, token) {
+          return $http.put('/api/emailverify/:username/:token').success(function (data) {
+          });
+      },
+      updatePassword: function updatePasswordMethod(user, name, token) {
+          return $http.put('/api/resetPassword/'+ name + '/' + token).success(function (data) {
+            console.log('Success!');
+          });
+      }
 
-app.factory('resetPassword',['$http','$window', function ($http, $window) {
-  var o = {};
-  o.reset = function (user, name, token) {
-    return $http.get('/resetPassword/'+ name + '/' + token).success(function (data) {
-      return data;
-    });
   };
-  o.updatePassword  = function (user, name, token) {
-    return $http.put('/api/resetPassword/'+ name + '/' + token).success(function (data) {
-      console.log('Success!');
-    });
-  };
-  return o;
-}]);
+});
--- client/set/js/models.js
+++ client/set/js/models.js
@@ -1,23 +1,19 @@
-// app.factory('confirmEmail',['$http', '$window', function ($http, $window) {
-//   var confirmEmail = {};
-//   confirmEmail.confirm = function (user) {
-//     return $http.put('/api/emailverify/:username/:token').success(function (data) {
-//     });
-//   };
-//   return confirmEmail;
-// }]);
+app.factory('verification', function ($http, $window) {
+  return {
+      getUser: function getUserMethod(user, name, token) {
+          return $http.get('/resetPassword/'+ name + '/' + token).success(function (data) {
+            return data;
+          });
+      },
+      emailVerify: function emailVerifyMethod(user, name, token) {
+          return $http.put('/api/emailverify/:username/:token').success(function (data) {
+          });
+      },
+      updatePassword: function updatePasswordMethod(user, name, token) {
+          return $http.put('/api/resetPassword/'+ name + '/' + token).success(function (data) {
+            console.log('Success!');
+          });
+      }
 
-app.factory('resetPassword',['$http','$window', function ($http, $window) {
-  var o = {};
-  o.reset = function (user, name, token) {
-    return $http.get('/resetPassword/'+ name + '/' + token).success(function (data) {
-      return data;
-    });
   };
-  o.updatePassword  = function (user, name, token) {
-    return $http.put('/api/resetPassword/'+ name + '/' + token).success(function (data) {
-      console.log('Success!');
-    });
-  };
-  return o;
-}]);
+});
