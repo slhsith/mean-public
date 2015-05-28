b43984fe92ba3d8400ea53e162f1f3b49c26616a
Sandy <slhsith@gmail.com>
2015-05-28 15:13:25 -0700

    recieved i before e

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -5,7 +5,7 @@ module.exports = function (io) {
 
     socket.on('message', function (from, msg) {
 
-      console.log('recieved message from', from, 'msg', JSON.stringify(msg));
+      console.log('received message from', from, 'msg', JSON.stringify(msg));
 
       console.log('broadcasting message');
       console.log('payload is', msg);
