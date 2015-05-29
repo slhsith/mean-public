3142eccfc9be659a75d363ce0fdc05066573bac4
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-05-29 11:37:33 -0400

    updates & gulp

--- app.js
+++ app.js
@@ -103,48 +103,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-/*
- * Respond to GET requests to /sign_s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and the
- * anticipated URL of the image.
- */
-app.get('/sign_s3', function(req, res){
-    aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-    var s3 = new aws.S3(); 
-    var s3_params = { 
-        Bucket: S3_BUCKET, 
-        Key: req.query.file_name, 
-        Expires: 60, 
-        ContentType: req.query.file_type, 
-        ACL: 'public-read'
-    }; 
-    s3.getSignedUrl('putObject', s3_params, function(err, data){ 
-        if(err){ 
-            console.log(err); 
-        }
-        else{ 
-            var return_data = {
-                signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name 
-            };
-            res.write(JSON.stringify(return_data));
-            res.end();
-        } 
-    });
-});
-
-/*
- * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in 
- * a way that suits your application.
- */
-app.post('/submit_form', function(req, res){
-    username = req.body.username;
-    full_name = req.body.full_name;
-    avatar_url = req.body.avatar_url;
-    update_account(username, full_name, avatar_url); // TODO: create this function
-    // TODO: Return something useful or redirect
-});
 // error handlers
 
 // development error handler
