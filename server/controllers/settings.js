433e64d2747ef7c87c7a7e10573e6c5384cfb2d9
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-05-29 15:46:21 -0400

    fix?

--- server/controllers/settings.js
+++ server/controllers/settings.js
@@ -11,11 +11,6 @@ var
   Language      = mongoose.model('Language'),
   Follower      = mongoose.model('Follower');
 
-//aws secret keys -- available in the .env and on heroku
-
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
 
 // --- Exported Methods --- //
 exports.getLanguages = function (req, res, next) {
@@ -50,30 +45,35 @@ exports.updateSettings = function (req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
       return res.status(200).json({message: 'Profile Updated!'});
-    }).success(aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-        var s3 = new aws.S3(); 
-        var s3_params = { 
-            Bucket: S3_BUCKET, 
-            Key: req.query.file_name, 
-            Expires: 60, 
-            ContentType: req.query.file_type, 
-            ACL: 'public-read'
-        }; 
-        s3.getSignedUrl('putObject', s3_params, function(err, data){ 
-            if(err){ 
-                console.log(err); 
-            }
-            else{ 
-                var return_data = {
-                    signed_request: data,
-                    url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name 
-                };
-                res.write(JSON.stringify(return_data));
-                res.end();
-            } 
-    }));
+    });
   });
 };
+
+//s3
+exports.sign_s3 = function (req, res, next) {
+  aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+  var s3 = new aws.S3(); 
+  var s3_params = { 
+      Bucket: S3_BUCKET, 
+      Key: req.query.file_name, 
+      Expires: 60, 
+      ContentType: req.query.file_type, 
+      ACL: 'public-read'
+  }; 
+  s3.getSignedUrl('putObject', s3_params, function(err, data){ 
+      if(err){ 
+          console.log(err); 
+      }
+      else{ 
+          var return_data = {
+              signed_request: data,
+              url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name 
+          };
+          res.write(JSON.stringify(return_data));
+          res.end();
+      } 
+  });
+}
 
 //search
 
